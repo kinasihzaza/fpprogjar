4f5fb974db7c6c7de9b5e31fc96a8ba9eeb2e817
kinasihzaza <lovely.zaza@gmail.com>
2016-01-05 10:02:32 +0700

    server terbaru

--- server.py
+++ server.py
@@ -5,6 +5,9 @@ import sys
 import signal
 import threading
 import time
+import platform
+import datetime
+import os.path
 import urllib2 #Transform URL string into normal string in python (%20 to space etc)
 
 ip = 'localhost'
@@ -21,6 +24,12 @@ f3 = str("./saddam.html")
 f4 = str("./dhea.html")
 
 f5 = str("./ucup.html")
+
+#local variabel
+os1 = platform.system()
+os2 = platform.release()
+ver = sys.version
+tgl = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
 
 class Server:
     def shutdown(self):
@@ -86,147 +95,192 @@ class Client(threading.Thread):
     def run(self):
         running = 1
         data = self.client.recv(self.size)
-        print data
+        print "Requested : "+data
         tempo = data.split()
+        #print tempo
+        print "METHOD : "+ tempo[0]
+        method = tempo[0]
         namaFile = tempo[1]
         namaFile = namaFile[1:]
         #print namaFile
         namaFile=urllib2.unquote(namaFile)
+        #IF METHOD GET
+        if(method == "GET"):
+            if os.path.isfile(namaFile):
+                print "FILENYA ADALAH : "+ str(namaFile)
+                if(namaFile.rfind('.php')>=0) or (namaFile.rfind('.html')>=0):
+                    if(namaFile.rfind('.php')>=0):
+                        
+                        commandPHP="php -f " + namaFile+" >temp.html"
+                        os.system(commandPHP) 
+                        dataTemp=open("temp.html","r")
+                        os.system('rm -rf temp.html')
 
-        if os.path.isfile(namaFile):
-            print "zazadisini : "+ str(namaFile)
-            if(namaFile.rfind('.php')>=0) or (namaFile.rfind('.html')>=0):
-                if(namaFile.rfind('.php')>=0):
-                    
-                    commandPHP="php -f " + namaFile+" >temp.html"
-                    os.system(commandPHP) 
-                    dataTemp=open("temp.html","r")
-                    os.system('rm -rf temp.html')
-
-                else:
-                    if(namaFile == "saddam.html"):
-                        dataTemp = open(f3,"r")
                     else:
-                        dataTemp = open(namaFile,"r")
+                        if(namaFile == "saddam.html"):
+                            dataTemp = open(f3,"r")
+                        else:
+                            dataTemp = open(namaFile,"r")
 
-                if(namaFile == "saddam.html"):
-                    self.client.send("HTTP/1.1 301 Moved Permanently\nContent-Type: text/html;\r\n\r\n")
-                
-                elif (namaFile == "dhea.html"):
-                    self.client.send("HTTP/1.1 403 Forbidden\nContent-Type: text/html;\r\n\r\n")
+                    if(namaFile == "saddam.html"):
+                        self.client.send("HTTP/1.1 301 Moved Permanently\nContent-Type: text/html\nContent-Language: id\r\n\r\n")
+                    
+                    elif (namaFile == "dhea.html"):
+                        self.client.send("HTTP/1.1 403 Forbidden\nContent-Type: text/html\nContent-Language: id\r\n\r\n")
 
-                elif (namaFile == "ucup.html"):
-                    self.client.send("HTTP/1.1 500 Internal Server Error\nContent-Type: text/html;\r\n\r\n")
+                    elif (namaFile == "ucup.html"):
+                        self.client.send("HTTP/1.1 500 Internal Server Error\nContent-Type: text/html\nContent-Language: id\r\n\r\n")
 
-                else:    
-                    self.client.send("HTTP/1.1 200 OK\nContent-Type: text/html;\r\n\r\n")
+                    else:    
+                        self.client.send("HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Language: id\r\n\r\n")
 
-                while 1:
-                    dataR = dataTemp.read(4096)
-                    print dataR
-                    self.client.send(dataR)
-                    if dataR == "":
-                        break
-            else:
-                self.client.send('HTTP/1.1 200 OK\nAccept-Ranges: bytes\nConnection: Keep-Alive\nContent-Type: application/x-gzip\r\n\r\n')
-                data=""
-                files = open(namaFile,"r")
-                while 1: 
-                    dataTemp = files.read(4096)
-                    self.client.send(dataTemp)
-                    if dataTemp=="":
-                        break
-        elif os.path.isdir(namaFile) or namaFile == "":
-            self.client.send("HTTP/1.1 200 OK\nContent-Type: text/html;\r\n\r\n")
-            if namaFile == "":
-                namaFile = "."
-            dataTemp = os.listdir(namaFile)
-            line= "---------------------------------"
-            print line
-            print "List Dir :"
-            print line
-            number = 0
-            number2=len(dataTemp)
-            for ahay in dataTemp:
-                if (number==number2):
-                    continue
-                print ">> "+dataTemp[number]
-                number = number +1
-
-            if "index.html" in dataTemp or "index.php" in dataTemp:
-                if "index.html" in dataTemp:
-                    dataTemp = open("./home/index.html","r")
+                    while 1:
+                        dataR = dataTemp.read(4096)
+                        #print dataR
+                        self.client.send(dataR)
+                        if dataR == "":
+                            break
                 else:
-                    tempE  = "php -f index.php > temp.html"
-                    os.system(tempE)
-                    dataTemp = open("temp.html","r")
-                    os.system("rm -rf temp.html")
-                while 1:
-                    dataR = dataTemp.read(4096)
-                    print dataR
-                    self.client.send(dataR)
-                    if dataR=="":
-                        break
-            else: #list Dir
-                index=len(dataTemp)
-                namafolder = namaFile.split("/")
-                print namafolder
-                namafolder[0]
-                head = '''
-                    <html>
-                    <head>
-                    <title>Index of /%s...</title>
-                    </head>
-                    <body>
-                    <h1>Index of /%s...</h1>
-                    <table>
-                    <tr>
-                    <td><a href="/%s"><font color = "green">Parent Directory</font></a></td>
-                    <tr><th colspan="200"><hr></th></tr>
-                    <tr><td valign="top"></td>
-                    </tr>
-                    <tr>
-                    <td><p><strong>Name<strong></p></td>
-                    <td><p><strong>Size(Byte)<strong></p></td>
-                    <td><p><strong>&emsp;Last Modified<strong></p></td>
-                    </tr>
-                    <tr><th colspan="200"><hr></th></tr>
-                    <tr><td valign="top"></td>
-                    
-                    '''%(namaFile,namaFile,namafolder[0])
-                base = "./"
+                    self.client.send('HTTP/1.1 200 OK\nAccept-Ranges: bytes\nConnection: Keep-Alive\nContent-Type: application/x-gzip\nContent-Language: id\r\n\r\n')
+                    data=""
+                    files = open(namaFile,"r")
+                    while 1: 
+                        dataTemp = files.read(4096)
+                        self.client.send(dataTemp)
+                        if dataTemp=="":
+                            break
+            elif os.path.isdir(namaFile) or namaFile == "":
+                self.client.send("HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Language: id\r\n\r\n")
+                if namaFile == "":
+                    namaFile = "."
+                dataTemp = os.listdir(namaFile)
+                line= "---------------------------------"
+                print line
+                print "List Dir :"
+                print line
+                number = 0
+                number2=len(dataTemp)
+                for ahay in dataTemp:
+                    if (number==number2):
+                        continue
+                    print ">> "+dataTemp[number]
+                    number = number +1
 
-                for i in range(0,index):
-                    filePath=base+namaFile+"/"+dataTemp[i]
-                    sizeFile=os.path.getsize(filePath)
-                    modifiedFile=time.ctime(os.path.getmtime(filePath))
-                    
-                    if(sizeFile==4096):
-                        sizeFile = "~"
-                    body = '''
-                    <tr>
-                    <td><a href='/%s/%s'>%s</a></td>
-                    <td>%s</td>
-                    <td>&emsp;%s</td>
-                    </tr>
-                    '''%(namaFile,dataTemp[i],dataTemp[i],sizeFile,modifiedFile)
-                    head = head +body
-                tail = '''
-                </table>
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                <address> ZazaSaddamDhitaDhea Server Running On (%s / %s) </adress>
-                '''%(ip, portConf)
-                
-                self.client.send(head)
-                self.client.send(tail)
-        else:
-            print "File Requested : "+namaFile+" not Found on server"
-            x = "HTTP/1.1 404 Not Found\nContent-Type: text/html;\r\n\r\n"
-            y = errorRespond
-            z = str(x)+str(y)
-            self.client.send(z)
-            dataTemp = errorRespond
+                if "index.html" in dataTemp or "index.php" in dataTemp:
+                    if "index.html" in dataTemp:
+                        dataTemp = open("./home/index.html","r")
+                    else:
+                        tempE  = "php -f index.php > temp.html"
+                        os.system(tempE)
+                        dataTemp = open("temp.html","r")
+                        os.system("rm -rf temp.html")
+                    while 1:
+                        dataR = dataTemp.read(4096)
+                        #print dataR
+                        self.client.send(dataR)
+                        if dataR=="":
+                            break
+                else: #list Dir
+                    index=len(dataTemp)
+                    namafolder = namaFile.split("/")
+                    print namafolder
+                    namafolder[0]
+                    head = '''
+                        <html>
+                        <head>
+                        <title>Index of /%s...</title>
+                        </head>
+                        <body>
+                        <h1>Index of /%s...</h1>
+                        <table>
+                        <tr>
+                        <td><a href="/%s"><font color = "green">Parent Directory</font></a></td>
+                        <tr><th colspan="200"><hr></th></tr>
+                        <tr><td valign="top"></td>
+                        </tr>
+                        <tr>
+                        <td><p><strong>Name<strong></p></td>
+                        <td><p><strong>Size(Byte)<strong></p></td>
+                        <td><p><strong>&emsp;Last Modified<strong></p></td>
+                        </tr>
+                        <tr><th colspan="200"><hr></th></tr>
+                        <tr><td valign="top"></td>
+                        
+                        '''%(namaFile,namaFile,namafolder[0])
+                    base = "./"
 
+                    for i in range(0,index):
+                        filePath=base+namaFile+"/"+dataTemp[i]
+                        sizeFile=os.path.getsize(filePath)
+                        modifiedFile=time.ctime(os.path.getmtime(filePath))
+                        
+                        if(sizeFile==4096):
+                            sizeFile = "~"
+                        body = '''
+                        <tr>
+                        <td><a href='/%s/%s'>%s</a></td>
+                        <td>%s</td>
+                        <td>&emsp;%s</td>
+                        </tr>
+                        '''%(namaFile,dataTemp[i],dataTemp[i],sizeFile,modifiedFile)
+                        head = head +body
+                    tail = '''
+                    </table>
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                    <address> ZazaSaddamDhitaDhea Server Running On (%s / %s) </adress>
+                    '''%(ip, portConf)
+                    
+                    self.client.send(head)
+                    self.client.send(tail)
+            else:
+                print "File Requested : "+namaFile+" not Found on server"
+                sizeleng = os.path.getsize('saddam.html')
+                x = "HTTP/1.1 404 Not Found\nAccept-Ranges: bytes\nConnection: Keep-Alive\nContent-Type: application/x-gzip\nContent-Type: text/html\nServer: Python "+str (ver)+str (os2)+str (os1)+"\nContent-Length: "+str(sizeleng)+"\nDate: "+str(tgl)+"\nContent-Language: id \n \r\n\r\n"
+                y = errorRespond
+                z = str(x)+str(y)
+                self.client.send(z)
+                dataTemp = errorRespond
+        #IF METHOD POST
+        elif(method == "POST"):
+            self.client.send('HTTP/1.1 200 OK\nAccept-Ranges: bytes\nConnection: Keep-Alive\nContent-Type: application/x-gzip\nContent-Language: id\r\n\r\n')
+   #        print "inidata" +data
+            
+            m = data.split ("\r\n\r\n")
+            requestHeaderPostMethod = m[0]
+            postValue = m[1]
+            print "ini post value : " +postValue
+            s = postValue.split ("&")
+            var1 = s[0]
+            var2 = s[1]
+            var1sesungguhnya = var1.split ("=")
+            var1sesungguhnya = var1sesungguhnya[1]
+            var2sesungguhnya = var2.split ("=")
+            var2sesungguhnya = var2sesungguhnya[1]
+            hasilJumlah = str (var1sesungguhnya) + str (var2sesungguhnya)
+            print "hasil post " +str (hasilJumlah)
+            x = namaFile
+            #if(namaFile.split(""))
+#            print "ini x" +x
+            files = open(x,"r")
+ #           print "xoxo"
+            data=""
+            self.client.send (str (hasilJumlah)+"\n")
+       #     while 1: 
+   #             dataTemp = files.read(4096)
+  #              print dataTemp
+    #            self.client.send(hasilJumlah)
+     #           if dataTemp=="":
+      #              break  
+        elif(method == "HEAD"):   
+            self.client.send("HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Language: id\r\n\r\n")
+            print "FILENYA ADALAH : "+ str(namaFile)
+            dataTemp = open(namaFile, 'r')
+            while 1:
+                dataR = dataTemp.read(4096)
+                #print dataR
+                self.client.send(dataR)
+                if dataR == "":
+                    break
         self.client.close();
 
 
